3202b2441fce8553a2b10ee6c83ac0a654d8c211
Spencer Schack <scs1818@gmail.com>
2017-08-11 17:30:55 -0700

    removed old files

--- client/app/helpers/add.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function add([a, b]) {
-  return a + b;
-}
-
-export default Ember.Helper.helper(add);
--- client/app/transitions.js
+++ client/app/transitions.js
@@ -1,12 +1,3 @@
 export default function() {
 
-  const duration = 500;
-
-  this.transition(
-    this.hasClass('game--play--outlet'),
-    this.toRoute('game.play.index'),
-    this.fromRoute('game.play.players'),
-    this.use('toRight', {duration}),
-    this.reverse('toLeft', {duration}));
-
 };
